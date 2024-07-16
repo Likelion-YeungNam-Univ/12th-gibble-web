5542d1b7cb825e87f1479b97ce218ef97ed93a95
hky035 <rldud035@naver.com>
2024-07-16 23:03:24 +0900

    feat: axios 전역 인스턴스 분류 - 일반 요청 / Authorization 헤더 필요 요청

--- gible/src/api/axios.js
+++ gible/src/api/axios.js
@@ -1,6 +1,56 @@
 import axios from 'axios'
+import { apiServer } from '../config/api'
+import store from '@store/store';
+import { setAccessToken, setRefreshToken } from '@store/store'
 
-export default axios.create({
-    baseURL : 'http://1.1.1.1'
+const BASE_URL = apiServer;
+
+export const apiClient = axios.create({
+    baseURL: BASE_URL
 })
 
+export const authApiClient = axios.create({
+    baseURL : BASE_URL
+})
+
+authApiClient.interceptors.request.use(
+    (config) => {
+        const state = store.getState();
+        const accessToken = state.authSlice.accessToken;
+        if (accessToken)
+            config.headers.Authorization = `Bearer ${accessToken}`;
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+)
+
+authApiClient.interceptors.response.use(
+    (response) => { 
+        return response;
+    },
+    async (error) => {
+        const originalRequest = error.config;
+        if (error.response.status == 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            try {
+                const state = store.getState();
+                const refreshToken = state.authSlice.refreshToken;
+                const response = await axios.post('/api/token/refresh', {
+                    refreshToken
+                });
+                const { accessToken, newRefreshToken } = response.data;
+                store.dispatch(setAccessToken(accessToken));
+                store.dispath(setRefreshToken(newRefreshToken));
+                originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+                return axios(originalRequest);
+            } catch (err) {
+                store.dispatch(logout());
+                return Promise.reject(err);
+            }
+        }
+        return Promise.reject(error);
+    }
+)
