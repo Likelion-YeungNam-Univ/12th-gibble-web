5754721f612735cd43876979ac50dabf6570065c
hky035 <rldud035@naver.com>
2024-07-17 03:26:02 +0900

    fix: import 변경

--- gible/src/store/store.js
+++ gible/src/store/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authSlice from './authSlice'
 
 const store = configureStore({
     reducer: {
-        authSlice: authSlice.reducer
+        auth: authSlice.reducer
     }
 });
 
