dd946dc66bb57ebddb389e17c5c3b43ff7b59fbe
hky035 <rldud035@naver.com>
2024-07-18 03:18:43 +0900

    feat: 헤더 컴포넌트 작성

---
+++ gible/src/layouts/header/SearchBox.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import styled from 'styled-components'
+import { ReactComponent as Search} from '@/assets/search.svg'
+
+const SearchBox = () => {
+  return (
+      <Wrapper>
+          <Input placeholder='검색어를 입력해주세요'></Input>
+          <Search/>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+    width: 230px;
+    height : 40px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    
+    box-sizing: border-box;
+    padding: 10px 20px;
+    background-color : #f6f6f6;
+    border-radius : 5px;
+    cursor: pointer;
+`
+const Input = styled.input`
+    outline : none;
+    border: none;
+    background : none;
+    font-size : 15px;
+    font-family : Pretendard;
+    width : 80%;
+
+`;
+
+export default SearchBox
