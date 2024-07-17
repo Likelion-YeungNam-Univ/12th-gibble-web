8255d675f2b46188af2b4b439b6ef76b903e2a39
hky035 <rldud035@naver.com>
2024-07-18 03:18:54 +0900

    feat: 푸터 컴포넌트 작성

--- gible/src/layouts/footer/Footer.js
+++ gible/src/layouts/footer/Footer.js
@@ -1,62 +1,46 @@
 import React from 'react';
 import styled from 'styled-components';
+import Description from './Description';
+import Notice from './Notice';
 
 
 const Footer = () => {
     return (
-        <FooterBox>
-            <Explain>
-                <Buttons>
-                    <button>개인정보처리방침</button>
-                    <button>이용약관</button>
-                    <button>이메일 무단 수집거부</button>
-                </Buttons>
-                <Number>
-                    <h1>0000-0000</h1>
-                    <h7>월~금 9:00 ~ 18:00</h7>
-                </Number>
-            </Explain>
-        </FooterBox>
+        <Wrapper>
+            <Logo/>
+            <DescContainer>
+                <Description/>
+                <Notice />
+            </DescContainer>
+        </Wrapper>
     );
 }
 
+const Wrapper = styled.div`
+    height : 200px;
+    background-color : #343434;
+    display : flex;
+    gap : 30px;
+    box-sizing : border-box;
+    padding : 30px 250px;
+    width : 100%;
+    position : absolute;
+    bottom : 0;
+`
 
-const FooterBox = styled.div`
-    background: #F6F6F6;
-    height: 262px; 
-`;
+const Logo = styled.div`
+    width : 220px;
+    height : 85px;
+    background-color : #f1f1f1;
+`
 
-const Explain = styled.div`
-    color: #767676;
-    display: flex;
-    justify-content: center;
-    justify-content: space-between;
-    padding: 80px;
-    margin: 0px 300px
+const DescContainer = styled.div`
+    display : flex;
+    justify-content : space-between;
+    width : 100%;
+`
 
-`;
 
-const Buttons =styled.div`
-    display: flex;
-    gap : 40px;
-    button {
-    
-        background: none; 
-        border: none; 
-        color: #767676; 
-        font-size: 15px; 
-        cursor: pointer; 
-
-        /*포인터 올릴 시에 밑줄*/
-        &:hover {
-            text-decoration: underline; 
-        } 
-`;
-
-const Number = styled.div`
-    text-align: right;
-    background: #F6F6F6;
-`;
 
 
 export default Footer;
