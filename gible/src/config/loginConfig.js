8fa2a8e90c91e7652e902e0e1f0514619594daa2
hky035 <rldud035@naver.com>
2024-07-17 20:27:53 +0900

    feat: login시 필요 환경 변수 설정

---
+++ gible/src/config/loginConfig.js
@@ -0,0 +1,7 @@
+const loginConfig = {
+    REST_API_KEY: process.env.REACT_APP_REST_API_KEY,
+    REDIRECT_URI: process.env.REACT_APP_REDIRECT_URI,
+    CLIENT_ID: process.env.REACT_APP_CLIENT_ID
+}
+
+export default loginConfig;
