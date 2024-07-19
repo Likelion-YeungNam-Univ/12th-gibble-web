cf5796e67e9e3229043143cc58de41400da93957
hky035 <rldud035@naver.com>
2024-07-19 19:09:53 +0900

    feat: main-color 전역 변수 추가

--- gible/src/GloablStyle.js
+++ gible/src/GloablStyle.js
@@ -1,6 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+    :root {
+        --main-color : #F42E3D;
+    }
     body {
         font-family : pretendard;
     }
