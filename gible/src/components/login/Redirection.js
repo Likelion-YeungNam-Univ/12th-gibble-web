ff8edb63faa96cc7b4fd5d277758dd826760bf59
hky035 <rldud035@naver.com>
2024-07-20 15:53:11 +0900

    feat: 로그인 response에 따른 라우팅 추가

--- gible/src/components/login/Redirection.js
+++ gible/src/components/login/Redirection.js
@@ -1,32 +1,46 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import loginConfig from '@/config/loginConfig'
-import apiClient from '@/api/axios';
 import Loading from '@/layouts/Loading';
+import loginResponseHandler from '@/api/login/loginResponseHandler';
+import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
+
 
 const Redirection = () => {
     const code = new URL(document.location.toString()).searchParams.get('code');
-    const navigate = useNavigate();
     const [isLoaded, setIsLoaded] = useState(false);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
   
+    const HandleloginResponse = async () => {
+      const result = await loginResponseHandler(dispatch, code);
+      if (result.statusCode === 200) {    
+        setIsLoaded(true);
+        navigate('/');
+      }
+      else if (result.statusCode === 510)
+        navigate('/signup');
+      else
+        navigate('/error');
+    }
+
     useEffect(() => {
-        /*
-          apiClient.post('/kakaologin', {
-            clientId : loginConfig.REST_API_KEY,
-            code
-          })
-        */
-      
-        // response받아서 setIsLoaded 호출
-      alert('loginSucceed');
-      navigate('/');
-    }, [])
+      HandleloginResponse();
+    },[])
+
 
   return (
-      <div>
+      <Wrapper>
         {(!isLoaded) ? <Loading /> : <div> </div>}
-      </div>
+      </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  width : 100%;
+  display : flex;
+  justify-content : center;
+  align-items : center;
+`;
 
 export default Redirection
