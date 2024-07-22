55929a68b36f75fbb3761049c5f97f54e7725bae
hky035 <rldud035@naver.com>
2024-07-23 02:12:53 +0900

    feat: 공통 컴포넌트 생성

---
+++ gible/src/components/common/Button.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+const ButtonComponent = styled.button`
+  display: inline-flex;
+  box-sizing : border-box;
+  padding: 14px 24px;
+  min-width : 120px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 6px;
+  background: var(--main-color);
+  color: #fff;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: var(--18, 26px); /* 144.444% */
+  border: none;
+  font-family : pretendard;
+  cursor: pointer;
+
+  ${props => props.$customStyles && css`
+    ${props.$customStyles}
+  `}
+`;
+
+const Button = ({ text, onClick, customStyles }) => {
+  return (
+    <ButtonComponent onClick={onClick} $customStyles={customStyles}>
+      {text}
+    </ButtonComponent>
+  );
+};
+
+export default Button;
