0f02dd884e40434108fc452321b32cd46548bd01
hky035 <rldud035@naver.com>
2024-07-25 22:21:01 +0900

    feat: loginHandler 수정 - refreshToken 삭제, response body clientId 삭제

--- gible/src/api/login/loginResponseHandler.js
+++ gible/src/api/login/loginResponseHandler.js
@@ -1,33 +1,22 @@
 import apiClient from "@/api/axios";
-import loginConfig from "@/config/loginConfig";
-import { setAccessToken, setRefreshToken } from "@/store/authSlice";
+import { setAccessToken } from "@/store/authSlice";
 import store from '@/store/store';
 
 const loginResponseHandler = async (dispatch, code) => {
     
     try {
         const response = await apiClient.post('auth/kakaologin', {
-            clientId: loginConfig.REST_API_KEY,
             code
         });
         console.log('response', response);
         console.log('responseStatus',response.status);
 
-        if (response.data.statusCode === 510) { // 추가 정보 필요
-            return {
-                statusCode: response.data.statusCode,
-                // message
-            }
-        }
-        else {                                  // 로그인
-            dispatch(setAccessToken(response.data.accessToken));
-            dispatch(setRefreshToken(response.data.refreshToken));
-            const state = store.getState();
-            console.log(state);
-            return {
-                statusCode: response.status,
-                // message
-            }
+        dispatch(setAccessToken(response.data.accessToken));
+        const state = store.getState();
+        console.log(state);
+        return {
+            statusCode: response.status,
+            // message
         }
 
     } catch (error) {
