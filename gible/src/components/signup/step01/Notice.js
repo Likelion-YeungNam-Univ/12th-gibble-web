b7b729adec21473d8e352e7f21a1ba7d870e0c4f
gugitgugit <rnwnsgur28@gmail.com>
2024-07-25 22:02:56 +0900

    refact: 프리티어 방지 코드 수정

--- gible/src/components/signup/step01/Notice.js
+++ gible/src/components/signup/step01/Notice.js
@@ -4,14 +4,14 @@ import CheckBox from "../CheckBox";
 
 const Notice = () => {
   return (
-    <Box>
-      <AgreementBox>
-        <AgreementTitle>
+    <Wrraper>
+      <NoticeBox>
+        <Title>
           기블은 개인정보 보호법에 따라 회원가입을 위한 개인정보 수집 및 이용에
           대해 아래와 같이 안내 드립니다. 내용을 읽어보신 후 동의여부를 결정하여
           주시기 바랍니다.
-        </AgreementTitle>
-        <AgreementDescription>
+        </Title>
+        <Description>
           1. 개인정보 수집 및 이용 목적 <br />
           &nbsp;&nbsp;1. 회원관리
           <br />
@@ -38,31 +38,84 @@ const Notice = () => {
           혈액정보관리시스템 내<br />
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;이용자의 헌혈정보와 연계되어 서비스
           됩니다.
-        </AgreementDescription>
-      </AgreementBox>
-      <AgreementCheckBox>
+          <br />
+          &nbsp;&nbsp;5. 기타 민원 처리
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;개인정보 이용·제공 내역의 통지, 이용자의
+          질의/상담 내용이나 불편사항, 오류 개선 등의 처리를 위해 수집된
+          개인정보를 이용할 수 있습니다.
+          <br />
+          <br />
+          2. 개인정보 수집항목
+          <br />
+          이용자는 회원가입을 하지 않아도 전자문진, 헌혈의집 찾기, 혈액보유현황,
+          단체헌혈예약, 공지사항・홍보자료・이벤트현황 등의 확인, 헌혈버스 운영
+          현황 확인 등의 서비스를 이용하실 수 있습니다. 혈액관리본부는
+          홈페이지/레드커넥트 서비스 이용 및 회원가입을 위해 최소한의
+          개인정보만을 수집합니다.
+          <br />
+          혈액관리본부 홈페이지와 레드커넥트 회원정보는 통합 관리되어 둘 중
+          하나의 서비스에서 회원가입을 하셔도 모두 이용 가능합니다.
+          <br />
+          이용자가 헌혈기록확인, 헌혈예약, 검사결과 확인, 이벤트 응모 등의
+          서비스를 이용하기 위해서는 회원가입 후 “헌혈자 인증”을 해야 합니다.
+          헌혈자 인증에 따른 개인정보 이용에 관하여는 헌혈자 인증 시 별도 동의를
+          받고 있습니다.
+          <br />
+          회원가입을 위해 혈액관리본부가 이용자로부터 수집하는 개인정보는 아래와
+          같습니다.
+          <br />
+          &nbsp;&nbsp;&bull;&nbsp;&nbsp;필수항목 : 아이디, 비밀번호, 이름,
+          생년월일, 휴대폰번호, 이메일주소 (만 14세 미만의 경우) 법정대리인의 정
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;보(이름, 생년월일,
+          중복가입확인정보(DI), 휴대폰번호)
+          <br />
+          서비스 이용 과정에서 IP Address, 로그인 일시, 불량 이용 기록, 서비스
+          이용 기록, 기기정보(기기고유번호, OS버전, 모델명, 제조사 정보),
+          위치정보 등의 정보가 자동으로 생성되어 수집될 수 있습니다.
+          <br />
+          <br />
+          3. 개인정보의 보유 및 이용 기간 대한적십자사 혈액관리본부 홈페이지 및
+          레드커넥트의 회원정보는 가입일로부터 2년간 보유하며, 보유기간이 경과한
+          회원정보는 파기합니다. 단, 개인정보 수집·이용 재동의 절차를 거쳐
+          개인정보의 보유 및 이용기간을 연장할 수 있습니다.
+          <br />
+          홈페이지 및 레드커넥트를 통해 수집되고 처리되는 개인정보와는 별도로
+          혈액관리법 제12조(기록의 작성 등)에 따라 대한적십자사 혈액관리본부는
+          혈액관리업무에 관한 기록을 10년간 보존할 수 있습니다.
+          <br />
+          <br />
+          4. 이용자는 위와 같이 안내한 개인정보 수집 및 이용에 동의를 거부할
+          권리가 있으며, 동의를 거부할 경우 회원가입이 불가합니다. 본인은
+          개인정보보호법 제15조에 따라 개인정보 수집 및 이용에 대한 안내사항을
+          고지 받고 개인정보 처리에 동의합니다
+        </Description>
+      </NoticeBox>
+      <CheckBoxWrapper>
         <CheckBox type="checkbox" />
         <span>동의합니다.</span>
-      </AgreementCheckBox>
-    </Box>
+      </CheckBoxWrapper>
+    </Wrraper>
   );
 };
 
 export default Notice;
 
-const Box = styled.div`
-  margin : 20px 0;
-`;
+const Wrraper = styled.div``;
 
-const AgreementBox = styled.div`
+const NoticeBox = styled.div`
   border-radius: 6px;
   border: 1px solid #dbdbdb;
   background: #fff;
   box-shadow: 0px 4px 8px 0px rgba(0, 0, 0, 0.08);
-  padding: 20px;
+  padding: 30px 50px;
+  margin: 20px 0;
+  height: 458px;
+  overflow-y: scroll;
 `;
 
-const AgreementTitle = styled.div`
+const Title = styled.div`
   overflow: hidden;
   font-size: 20px;
   font-style: normal;
@@ -71,7 +124,7 @@ const AgreementTitle = styled.div`
   letter-spacing: -0.5px;
 `;
 
-const AgreementDescription = styled.div`
+const Description = styled.div`
   color: #767676;
   font-size: 20px;
   font-style: normal;
@@ -80,9 +133,9 @@ const AgreementDescription = styled.div`
   letter-spacing: -0.5px;
 `;
 
-const AgreementCheckBox = styled.div`
+const CheckBoxWrapper = styled.div`
   display: flex;
   justify-content: right;
-  margin: 10px 0px;
+  margin: 12px 0px;
   align-items: center;
 `;
