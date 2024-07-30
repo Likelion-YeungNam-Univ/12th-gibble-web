1b8cf281dd2e20afc5ef6421f95176da09ba27ba
Koo Jun Hyeok <164734665+gugitgugit@users.noreply.github.com>
2024-07-30 19:37:13 +0900

    feat: 일부 라우팅, 네비게이션 기능 추가 (#45)

--- src/components/post/post/Content.js
+++ src/components/post/post/Content.js
@@ -1,51 +1,52 @@
-import React from 'react'
-import PostCategory from './PostCategory'
-import PostList from './PostList'
-import styled from 'styled-components'
-import { ReactComponent as WriteButton} from '@/assets/svg/post-write-button.svg'
-import PageController from './PageController'
+import React from "react";
+import PostCategory from "./PostCategory";
+import PostList from "./PostList";
+import styled from "styled-components";
+import { ReactComponent as WriteButton } from "@/assets/svg/post-write-button.svg";
+import PageController from "./PageController";
+import { useNavigate } from "react-router-dom";
 
 const Content = () => {
+  const navigate = useNavigate();
   return (
-      <Wrapper>
-          <PostCategory /> 
-          <PostList />
-          <ButtonContainer>
-            <PageController/>
-              <WriteButtonContainer>
-                  <WriteBtn onClick={() => console.log('click') } />
-                </WriteButtonContainer>
-          </ButtonContainer>
-      </Wrapper>
-  )
-}
+    <Wrapper>
+      <PostCategory />
+      <PostList />
+      <ButtonContainer>
+        <PageController />
+        <WriteButtonContainer>
+          <WriteBtn onClick={() => navigate("/post/new")} />
+        </WriteButtonContainer>
+      </ButtonContainer>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
-    margin-bottom : 100px;
-
-`
+  margin-bottom: 100px;
+`;
 
 const ButtonContainer = styled.div`
-    height : 150px;
-    display : flex;
-    justify-content : center;
-    align-items: center;
-    box-sizing : border-box;
-    padding : 20px 0;
-    position : relative;
-`
+  height: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 20px 0;
+  position: relative;
+`;
 
 const WriteBtn = styled(WriteButton)`
-    cursor: pointer;
-`
+  cursor: pointer;
+`;
 
 const WriteButtonContainer = styled.div`
-    position : absolute;
-    right : 10px;
-    align-self : flex-end;
-    display : flex;
-    justify-content : center;
-    align-items : center;
-`
+  position: absolute;
+  right: 10px;
+  align-self: flex-end;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-export default Content
+export default Content;
--- src/layouts/header/Navbar.js
+++ src/layouts/header/Navbar.js
@@ -1,34 +1,34 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import styled from 'styled-components'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 const Navbar = () => {
   const navigate = useNavigate();
   return (
-      <Wrapper>
-          <Category>커뮤니티</Category>
-          <Category onClick={()=>navigate('/event')}>이벤트</Category>
-          <Category>사업 소개</Category>
+    <Wrapper>
+      <Category onClick={() => navigate("/post")}>커뮤니티</Category>
+      <Category onClick={() => navigate("/event")}>이벤트</Category>
+      <Category>사업 소개</Category>
     </Wrapper>
-  )
-}
+  );
+};
 
 const Wrapper = styled.div`
-    box-sizing : border-box;
-    padding : 20px 20%;
-    border-top : 1px solid #dbdbdb;
-    border-bottom : 1px solid #dbdbdb;
+  box-sizing: border-box;
+  padding: 20px 20%;
+  border-top: 1px solid #dbdbdb;
+  border-bottom: 1px solid #dbdbdb;
 
-    display : flex;
-    justify-content : space-between;
-    align-items : center
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 const Category = styled.div`
-    font-size : 20px;
-    color : #111;
-    font-weight: bold;
-    cursor: pointer;
-`
+  font-size: 20px;
+  color: #111;
+  font-weight: bold;
+  cursor: pointer;
+`;
 
-export default Navbar
+export default Navbar;
