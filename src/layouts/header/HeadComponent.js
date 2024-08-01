48504d8c167b315020a9800c77dd6c28f22705d6
hky035 <rldud035@naver.com>
2024-08-01 15:57:39 +0900

    chore: 불필요 import 삭제

--- src/layouts/header/HeadComponent.js
+++ src/layouts/header/HeadComponent.js
@@ -2,7 +2,6 @@ import React from 'react'
 import styled from 'styled-components'
 import SearchBox from './SearchBox';
 import { useNavigate } from 'react-router-dom';
-import store from '@/store/store';
 import logoutHandler from '@/api/login/logoutHandler';
 import { useDispatch, useSelector } from 'react-redux';
 
