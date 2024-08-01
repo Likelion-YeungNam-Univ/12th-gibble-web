4c003abc370123856ad2348f7f7ee04922fd5cfc
hky035 <rldud035@naver.com>
2024-08-01 15:59:07 +0900

    feat: getAllReviewByPageNum api-컴포넌트 연결

--- src/pages/review/Review.js
+++ src/pages/review/Review.js
@@ -1,62 +1,43 @@
 import PageNotice from '@/components/common/PageNotice'
 import Separator from '@/components/common/Separator'
 import ReviewCard from '@/components/review/ReviewCard'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import PageController from '../../components/review/PageController';
+import { useNavigate, useSearchParams } from 'react-router-dom'
+import getAllReviewByPageNum from '@/api/review/getAllReviewByPageNum'
 
 const Review = () => {
+  
+  const [reviewList, setReviewList] = useState([]);
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
 
-  const result = [
-    {
-      id: 'fjdsflacnl3nkflnl',
-      title: '너무 감사',
-      date: '2024.07.30'
-    },
-    {
-      id: 'fjdsflacnl3nkflnl',
-      title: '너무 감사',
-      date: '2024.07.30'
-    },
-    {
-      id: 'fjdsflacnl3nkflnl',
-      title: '너무 감사',
-      date: '2024.07.30'
-    },
-    {
-      id: 'fjdsflacnl3nkflnl',
-      title: '너무 감사',
-      date: '2024.07.30'
-    },
-    {
-      id: 'fjdsflacnl3nkflnl',
-      title: '너무 감사',
-      date: '2024.07.30'
-    },
-    {
-      id: 'fjdsflacnl3nkflnl',
-      title: '너무 감사',
-      date: '2024.07.30'
-    },
-    {
-      id: 'fjdsflacnl3nkflnl',
-      title: '너무 감사',
-      date: '2024.07.30'
-    },
-    {
-      id: 'fjdsflacnl3nkflnl',
-      title: '너무 감사',
-      date: '2024.07.30'
+  useEffect(()=> {
+    const fetchReview = async () => {
+      const page = searchParams.get('page') || 0;
+      console.log('pageNum',page)
+
+      try {
+        const result = await getAllReviewByPageNum({page});
+
+        console.log('result',result)
+        setReviewList(result.data.content);
+      } catch(error) {
+        console.log('error',error);
+        navigate('/error');
+      }
     }
-  ]
+    fetchReview();
+  }, [navigate, searchParams])
 
   return (
     <Wrapper>
         <PageNotice location={['Home','커뮤니티','투명 후기']} />
         <Separator title={'투명후기'} />
         <Container>
-          {result.map((el,idx) => {
-            return <ReviewCard review={el}></ReviewCard>
+          {reviewList.map(el => {
+            return <ReviewCard key={el.id} eview={el}></ReviewCard>
           })}
         </Container>
         <PageControllerContainer>
