461f0fd450c443ef18d4334c4f9e9b8d09b85477
hky035 <rldud035@naver.com>
2024-08-01 15:56:39 +0900

    feat: 페이지네이션 적용 api 추가

---
+++ src/api/review/getAllReviewByPageNum.js
@@ -0,0 +1,28 @@
+import { getAuthAxios } from "../authAxios";
+
+const getAllReviewByPageNum = async ({page}) => {
+    try{
+        const authAxios = getAuthAxios()
+        const response = await authAxios.get('/review',{
+            params : {page}
+        });
+        console.log('response',response)
+
+        if(response.status === 200){
+            console.log(response);
+            return {
+                statusCode : response.status,
+                data : response.data
+            }
+        }
+    }
+    catch(error){
+        return {
+            statusCode : error.response.status
+        }
+    }
+        
+
+}
+
+export default getAllReviewByPageNum
