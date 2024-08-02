0f3e04cae6e9451cb6df21e13a8ade02510e8ccf
Kiyoung Heo <128910345+hky035@users.noreply.github.com>
2024-08-03 02:36:33 +0900

    HF-119: 게시글 페이지네이션 구현 (#59)

--- src/components/post/post/PostList.js
+++ src/components/post/post/PostList.js
@@ -4,38 +4,72 @@ import PostCard from "./PostCard";
 import showPostList from "@/api/post/showPostList";
 import { useSearchParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import PageController from "./PageController";
+import { ReactComponent as WriteButton } from "@/assets/svg/post-write-button.svg";
 
 const PostList = () => {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
   const [postList, setPostList] = useState([]);
+  const [nowPage, setNowPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
 
   useEffect(() => {
     const fetch = async () => {
-      const page = searchParams.get("page") || 0;
-      console.log(page);
+      setNowPage(parseInt(searchParams.get("page")) || 0);
+
       const size = 10;
       try {
-        const result = await showPostList({ page, size });
+        const result = await showPostList({ nowPage, size });
 
-        console.log(result.data.content);
+        console.log(result);
         setPostList(result.data.content);
+        setTotalPages(result.totalPages);
       } catch (error) {
         console.log("error", error);
         navigate("/error");
       }
     };
     fetch();
-  }, [navigate, searchParams, setPostList]);
+  }, [nowPage, setNowPage]);
 
   return (
     <>
       {postList.map((el, index) => {
-        return <PostCard post={el} index={index + 1} />;
+        return <PostCard post={el} index={10 * nowPage + index + 1} />;
       })}
+      <ButtonContainer>
+        <PageController nowPage={nowPage} setNowPage={setNowPage} totalPages={totalPages}/>
+        <WriteButtonContainer>
+          <WriteBtn onClick={() => navigate("/post/new")} />
+        </WriteButtonContainer>
+      </ButtonContainer>
     </>
   );
 };
 
+const ButtonContainer = styled.div`
+  height: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 20px 0;
+  position: relative;
+`;
+
+const WriteBtn = styled(WriteButton)`
+  cursor: pointer;
+`;
+
+const WriteButtonContainer = styled.div`
+  position: absolute;
+  right: 10px;
+  align-self: flex-end;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default PostList;
