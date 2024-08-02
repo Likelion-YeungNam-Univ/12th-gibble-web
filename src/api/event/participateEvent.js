521fe34c50b96b278d4dbb52ea7d8c7d1985fa41
kyw2790 <165015015+kyw2790@users.noreply.github.com>
2024-08-03 04:27:14 +0900

    HF-59: Event API  (#63)
    * feat: 이벤트 API 1차 (지역 추가로 인한 수정필요)
    
    * feat: Event list 19번 수정완료
    
    * feat: eventAPI 수정완료 (지역)
    
    * feat: 이벤트 API

---
+++ src/api/event/participateEvent.js
@@ -0,0 +1,26 @@
+// 이벤트 참여
+import { getAuthAxios } from '../authAxios';
+export const participateEvent = async (eventId) => {
+  const authAxios = getAuthAxios();
+
+  try {
+    const response = await authAxios.post(`/event/${eventId}/participation`);
+
+    console.log("response", response);
+
+    if (response.status === 200) {
+      return {
+        statusCode: response.status,
+        message: response.data.message,
+      };
+    }
+    
+  } catch (error) {
+    return {
+      statusCode: error.response.status,
+      message: error.response.data.message,
+    };
+  }
+};
+
+export default participateEvent;
