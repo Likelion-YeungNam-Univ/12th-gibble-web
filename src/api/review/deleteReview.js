4a0fcb685690dc0ab9174d86998a6f1ed88a42a0
hky035 <rldud035@naver.com>
2024-08-03 17:05:35 +0900

    feat: 리뷰 삭제 api

---
+++ src/api/review/deleteReview.js
@@ -0,0 +1,20 @@
+import { getAuthAxios } from "../authAxios"
+
+const deleteReview = async (reviewId) => {
+    const authAxios = getAuthAxios();
+    try{
+        const response = await authAxios.delete(`/review/${reviewId}`);
+
+        return {
+            statusCode : response.status
+        }
+    } catch(error) {
+        console.error(error);
+        return {
+            statusCode : error.response.status
+        }
+    }
+    
+}
+
+export default deleteReview
