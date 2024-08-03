b650f4e718f9a0251c9e82e0ea19c8f9629e382c
kyw2790 <kyw2790@gmail.com>
2024-08-04 00:25:14 +0900

    feat: event 제목 삭제

--- src/components/mypage/Event.js
+++ src/components/mypage/Event.js
@@ -4,49 +4,41 @@ import profileImage from '../../assets/image/eventpost.png'; // 이미지 파일
 
 const events = [
     {
-      title: "라식글자수가많아지면밑으로내려갑니다이벤트",
       image: profileImage, 
       alt: "라식 이벤트",
       link: "https://www.youtube.com/" // 이벤트 링크
     },
     {
-      title: "영화이벤트",
       image: profileImage,
       alt: "영화이벤트",
       link: "https://example.com/2"
     },
     {
-      title: "무슨이벤트",
       image: profileImage,
       alt: "무슨이벤트",
       link: "https://example.com/3"
     },
     {
-      title: "과자이벤트",
       image: profileImage,
       alt: "과자이벤트",
       link: "https://example.com/4"
     },
     {
-      title: "이월드이벤트",
       image: profileImage,
       alt: "이월드이벤트",
       link: "https://example.com/5"
     },
     {
-      title: "asdf",
       image: profileImage,
       alt: "프로필 사진 6",
       link: "https://example.com/6"
     },
     {
-      title: "asdf",
       image: profileImage,
       alt: "프로필 사진 7",
       link: "https://example.com/7"
     },
     {
-      title: "asdf",
       image: profileImage,
       alt: "프로필 사진 8",
       link: "https://example.com/8"
@@ -74,7 +66,6 @@ const EventViewer = () => {
             <Event key={index}>
               <EventLink href={event.link} target="_blank" rel="noopener noreferrer">
                 <EventImage src={event.image} alt={event.alt} />
-                <Eventtitle>{event.title}</Eventtitle>
               </EventLink>
             </Event>
           ))}
@@ -138,16 +129,7 @@ const EventImage = styled.img`
     height: 230px;
 `;
 
-const Eventtitle = styled.p`
-    height: 55px;
-    color: #767676;
-    text-align: center;
-    font-size: 18px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: 26px; /* 144.444% */
-    letter-spacing: -0.45px;
-`;
+
 
 const EventLink = styled.a`
   text-decoration: none;
