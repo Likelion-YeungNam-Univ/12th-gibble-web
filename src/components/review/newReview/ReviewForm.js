25feeff00f62f8981a1cf5703bf889ded0f3ddb7
kyw2790 <kyw2790@gmail.com>
2024-08-04 21:50:36 +0900

    feat: 모달창 및 게시물 첨부 x => 테두리 생성

--- src/components/review/newReview/ReviewForm.js
+++ src/components/review/newReview/ReviewForm.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
 import InputLabel from "@/components/common/InputLabel";
@@ -12,19 +12,16 @@ import { v4 as uuid } from "uuid";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import newReview from "@/api/review/newReview";
 import { useNavigate } from "react-router-dom";
+import Modal from "./Modal";
 
 const ReviewForm = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm({
+  const { register, handleSubmit, watch, formState: { errors } } = useForm({
     mode: "onSubmit",
   });
 
   const fileInputRef = useRef(null);
   const [img, setImg] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const title = watch("title", "");
   const content = watch("content", "");
@@ -34,12 +31,20 @@ const ReviewForm = () => {
     document.getElementById("fileInput").click();
   };
 
+  const [selectedPostTitle, setSelectedPostTitle] = useState("");
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  const handlePostSelect = (title) => {
+    setSelectedPostTitle(title);
+  };
+
+
   return (
     <Wrapper
       onSubmit={handleSubmit(async (data) => {
-        // 게시글 링크 필요
-        if(!img)
-          return;
+        if (!img) return;
 
         const imageId = uuid();
         const storageRef = ref(storage, `review/${imageId}/${imageId}.png`);
@@ -48,50 +53,35 @@ const ReviewForm = () => {
           const snapshot = await uploadBytes(storageRef, img);
           const downloadUrl = await getDownloadURL(snapshot.ref);
 
-          console.log({...data, imageUrl : downloadUrl});
+          console.log({ ...data, imageUrl: downloadUrl });
 
           const result = await newReview({
-            title : data.title,
-            content : data.content,
-            imageUrl : downloadUrl,
+            title: data.title,
+            content: data.content,
+            imageUrl: downloadUrl,
             imageId,
-            postId : 'ae4eae75-22ec-4b82-b163-a9312a785d59' // 수정 필요
-          })
+            postId: "ae4eae75-22ec-4b82-b163-a9312a785d59", // 수정 필요
+          });
 
-          navigate('/review/new/uploaded');
+          navigate("/review/new/uploaded");
         } catch (error) {
-          console.error('file upload to firebase error', error);
+          console.error("file upload to firebase error", error);
         }
-        
       })}
     >
       <FormWrapper>
         <TextCount>{title.length}/30</TextCount>
         <InputWrapper>
-          <InputLabel
-            text={"제목"}
-            isEssential={true}
-            condition={"(30자 이하)"}
-          />
-
+          <InputLabel text={"제목"} isEssential={true} condition={"(30자 이하)"} />
           <Input
             type="text"
             placeholder="제목을 입력해 주세요."
             style={errors.title && { border: "1px solid var(--main-color)" }}
-            $customStyles={{
-              width: "81%",
-              height: "52px",
-              padding: "0 24px",
-            }}
+            $customStyles={{ width: "81%", height: "52px", padding: "0 24px" }}
             {...register("title", {
               required: "제목을 입력해주세요.",
-              minLength: {
-                value: 1,
-                message: "최소 1자 이상",
-              },
-              maxLength: {
-                value: 30,
-              },
+              minLength: { value: 1, message: "최소 1자 이상" },
+              maxLength: { value: 30 },
             })}
           />
         </InputWrapper>
@@ -101,57 +91,39 @@ const ReviewForm = () => {
 
         <TextCount>{content.length}/500</TextCount>
         <InputWrapper>
-          <InputLabel
-            text={"내용"}
-            isEssential={true}
-            condition={"(500자 이하)"}
-          />
+          <InputLabel text={"내용"} isEssential={true} condition={"(500자 이하)"} />
           <TextArea
             placeholder="어떠한 좋은 기적이 일어났는지 적어주세요"
             style={errors.content && { border: "1px solid var(--main-color)" }}
             {...register("content", {
               required: "내용을 입력해주세요.",
-              minLength: {
-                value: 1,
-                message: "최소 1자 이상",
-              },
-              maxLength: {
-                value: 500,
-                message: "최대 500자 이하",
-              },
+              minLength: { value: 1, message: "최소 1자 이상" },
+              maxLength: { value: 500, message: "최대 500자 이하" },
             })}
           />
         </InputWrapper>
-        {content.length > 500 ? (
-          <Error text={"500자 이하로 작성해주세요"} />
-        ) : null}
+        {content.length > 500 ? <Error text={"500자 이하로 작성해주세요"} /> : null}
         {errors.content && <Error text={errors.content.message} />}
         <ContentNotice />
 
         <InputFormFix text={"이름"} content={"홍길동"} />
 
-        {/* 게시글 링크 필요 !! */}
         <InputWrapper style={{ marginTop: "32px" }}>
-            
-            <InputLabel text={"게시글링크"} isEssential={true} />
-            <TmpContainer>
-              <Input
+          <InputLabel text={"게시글링크"} isEssential={true} />
+          <TmpContainer>
+            <Input
               type="text"
-              placeholder="어떤 게시글의 후기글인지 링크를 업로드해주세요"
+              placeholder="어떤 게시글의 후기글인지 게시글을 선택해 주세요"
               style={errors.link && { border: "1px solid var(--main-color)" }}
-              $customStyles={{
-                width: "400px",
-                height: "52px",
-                padding: "0 24px",
-                display: "flex",
-                flexShrink: "0",
-              }}
+              {...register("link", {
+                required: "게시글을 선택해 주세요.",
+              })}
+              $customStyles={{ width: "400px", height: "52px", padding: "0 24px", display: "flex", flexShrink: "0" }}
+              value={selectedPostTitle}
             />
             <Button
               type="button"
-              onClick={(e) => {
-                console.log('clicked')
-              }}
+              onClick={openModal}
               $customStyles={{
                 width: "150px",
                 background: "var(--main-color)",
@@ -163,60 +135,57 @@ const ReviewForm = () => {
                   color: "#fff",
                 },
               }}
-            >게시글 찾기</Button>
+            >
+              게시글 찾기
+            </Button>
           </TmpContainer>
-          
         </InputWrapper>
+        
+
 
         <InputWrapper style={{ marginTop: "32px" }}>
-          <InputLabel 
-            text={"인증사진 첨부"} 
-            isEssential={true} 
-          />
+          <InputLabel text={"인증사진 첨부"} isEssential={true} />
           <TmpContainer>
-          <input
-            id="fileInput"
-            type="file"
-            ref={fileInputRef}
-            style={{ display: "none" }}
-            accept="image/*"
-            onChange={(e) => setImg(e.target.files[0])}
+            <input
+              id="fileInput"
+              type="file"
+              ref={fileInputRef}
+              style={{ display: "none" }}
+              accept="image/*"
+              onChange={(e) => setImg(e.target.files[0])}
             />
-          <Input
-            type="text"
-            placeholder="이미지를 첨부해 주세요."
-            style={errors.image && { border: "1px solid var(--main-color)" }}
-            $customStyles={{
-              width: "400px",
-              height: "52px",
-              padding: "0 24px",
-              display: "flex",
-            }}
-            value={img && img.name }
-            readOnly
-          />
-          <Button
-            type="button"
-            onClick={(e) => {
-              e.preventDefault();
-              onClick();
-            }}
-            $customStyles={{
-              width: "150px;",
-              background: "var(--main-color)",
-              color: "#f4f4f4",
-              transition: "0.2s",
-              "&:hover": {
-                background: "var(--gray-color)",
-                color: "#fff",
-              },
-            }}
-          >
-            사진찾기
-          </Button>
+            <Input
+              type="text"
+              placeholder="이미지를 첨부해 주세요."
+              style={errors.image && { border: "1px solid var(--main-color)" }}
+              {...register("img", {
+                required: "이미지를 첨부해 주세요",
+              })}
+              $customStyles={{ width: "400px", height: "52px", padding: "0 24px", display: "flex" }}
+              value={img && img.name}
+              
+            />
+            <Button
+              type="button"
+              onClick={(e) => {
+                e.preventDefault();
+                onClick();
+              }}
+              $customStyles={{
+                width: "150px;",
+                background: "var(--main-color)",
+                color: "#f4f4f4",
+                transition: "0.2s",
+                "&:hover": {
+                  background: "var(--gray-color)",
+                  color: "#fff",
+                },
+              }}
+            >
+              사진찾기
+            </Button>
           </TmpContainer>
         </InputWrapper>
-        {errors.image && <Error text={errors.image.message} />}
         <ImageNotice />
       </FormWrapper>
       <Button
@@ -235,6 +204,10 @@ const ReviewForm = () => {
       >
         작성하기
       </Button>
+
+      <Modal isOpen={isModalOpen} onClose={closeModal} onPostSelect={handlePostSelect}>
+        <Button onClick={closeModal}>확인</Button>
+      </Modal>   
     </Wrapper>
   );
 };
@@ -279,9 +252,9 @@ const TextArea = styled.textarea`
 `;
 
 const TmpContainer = styled.div`
-  display : flex;
-  justify-content : space-between;
-  width : 81%;
-`
+  display: flex;
+  justify-content: space-between;
+  width: 81%;
+`;
 
 export default ReviewForm;
