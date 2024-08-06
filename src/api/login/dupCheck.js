868d6ce19580506e13a610a034c7636e0ac591b3
gugitgugit <rnwnsgur28@gmail.com>
2024-08-06 23:45:09 +0900

    fix: 이름, 닉네임 공백 포함 시 등록 불가

--- src/api/login/dupCheck.js
+++ src/api/login/dupCheck.js
@@ -2,10 +2,21 @@ import apiClient from "../axios";
 
 const dupCheck = async (nickname) => {
   try {
-
     console.log("전달된 닉네임 : ", nickname);
-    if(nickname === "" || nickname || nickname !== undefined){
-      return { statusCode : '555', message : '닉네임 입력은 필수입니다.'}
+    if (
+      nickname === "" ||
+      !nickname ||
+      nickname === undefined ||
+      nickname.trim() === ""
+    ) {
+      return { statusCode: "555", message: "닉네임이 양식에 맞지 않습니다." };
+    }
+
+    if (nickname.includes(" ")) {
+      return {
+        statusCode: "555",
+        message: "닉네임에 공백이 포함될 수 없습니다.",
+      };
     }
 
     const response = await apiClient.post("/user/dupCheck", null, {
